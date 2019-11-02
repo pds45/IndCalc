d5cddc781ead705d65fdcfa77d68e91c3eff2d36
priyankadharan <pds45@njit.edu>
2019-11-02 12:54:16 -0400

    Feature: Setup calculator with square

--- src/Calc.py
+++ src/Calc.py
@@ -18,28 +18,54 @@ def square(a):
     return a * a
 
 
+def squareroot(a):
+    return a ** 0.5
+
 class Calc:
-    result = 0
+    def my_function(self):
+        return 0
 
-    def __init__(self):
-        pass
+    if __name__ == "__main__":
+        a = float(input("Enter first number:"))
+        b = float(input("Enter second number:"))
+        print("1.Add")
+        print("2.Subtract")
+        print("3.Multiply")
+        print("4.Divide")
+        print("5.Square")
+        print("6.Sqroot")
+        choice = int(input("Enter your choice:1/2/3/4/5/6"))
 
-    def add(self, a, b):
-        self.result = addition(a, b)
-        return self.result
+    if choice == 1:
+     print(addition(a, b))
 
-    def subtract(self, a, b):
-        self.result = subtraction(a, b)
-        return self.result
+     #result = addition(a, b)
+        #return result
 
-    def mul(self, a, b):
-        self.result = multiply(a, b)
-        return self.result
+    elif choice == 2:
+     print(subtraction(a, b))
+     #result = subtraction(a, b)
+      #  return result
 
-    def div(self, a, b):
-        self.result = division(a, b)
-        return self.result
+    elif choice == 3:
+     print(multiply(a, b))
+     #result = multiply(a, b)
+      #  return result
 
-    def sq(self, a):
-        self.result = square(a)
-        return self.result
+    elif choice == 4:
+     print(division(a, b))
+     #result = division(a, b)
+      #  return result
+
+    elif choice ==5:
+     print(square(a))
+      #  result = square(a)
+       # return result
+
+    elif choice == 6:
+     print(squareroot(a))
+      #  result = squareroot(a)
+       # return result
+
+    else:
+     print("invalid choice")
